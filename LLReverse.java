a4c888a3ebc36ef1590aa38b8d03ae55d9f29c70
aman-goyal-007 <amangoyal007@gmail.com>
2017-02-24 11:40:30 +0530

    Changing to java file

---
+++ LLReverse.java
@@ -0,0 +1,35 @@
+
+public class LLReverse {
+
+	public static void main(String...args){
+		Node head = new Node(1, new Node(2,new Node(3,new Node(4,null))));
+		
+		Node current,previous,next;
+		current=head;
+		previous=null;
+		next=null;
+		while(current != null){
+			next = current.next;
+			current.next=previous;
+			previous=current;
+			current=next;
+		}
+		head=previous;
+		Node temp=head;
+		while(true){
+			System.out.println(temp.data);
+			if(temp.next==null) break;
+			else temp = temp.next;
+		}
+		System.out.println(" Done printing.");
+		
+	}
+}
+class Node{
+	int data;
+	Node next;
+	Node(int data,Node next){
+		this.data=data;
+		this.next=next;
+	}
+}
