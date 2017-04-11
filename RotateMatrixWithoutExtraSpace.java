a878304d7acc6588976b4c67b9f3ecc2c9b6c755
agoy22 <agoy22@DLUAGOY2260822.sapient.com>
2017-04-11 22:37:10 +0530

    Adding new algo

--- RotateMatrixWithoutExtraSpace.java
+++ RotateMatrixWithoutExtraSpace.java
@@ -1,4 +1,45 @@
 
 public class RotateMatrixWithoutExtraSpace {
 
+	static int length=0;
+	public static void main(String...args){
+		int[][] matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		length=matrix.length;
+		length--;
+		rotateBy90Degree(matrix);
+	}
+	
+	static void rotateBy90Degree(int[][] matrix){
+		System.out.println("Before Rotation");
+		printMatrix(matrix);
+		rotate(matrix,0,0,0,-1);
+		for(int i=0;i<=(length/2);i++){
+			for(int j=1;j<(length-i);j++){
+				rotate(matrix,i,j,0,-1);
+			}
+		}
+		System.out.println("After Rotation");
+		printMatrix(matrix);
+	}
+	static void rotate(int[][] matrix,int i,int j,int count,int data){
+		if(count==(length+1)){
+			matrix[i][j]=data;
+			return;
+		}
+		else{
+			int _i=length-j;
+			int _j=i;
+			int element=matrix[i][j];
+			rotate(matrix,_i,_j,++count,element);
+			matrix[_i][_j]=element;
+		}
+	}
+	static void printMatrix(int[][] matrix){
+		for(int i=0;i<=length;i++){
+			for(int j=0;j<=length;j++){
+				System.out.print(matrix[i][j]+"|");
+			}
+			System.out.println();
+		}
+	}
 }
--- Tree.java
+++ Tree.java
@@ -1,4 +1,78 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+/*
+ * Here we will perform all possible operations on tree
+ * LevelOrderTraversal
+ * LevelOrderTraversal in spiral
+ */
 public class Tree {
+	public static void main(String...args){
+		NodeTree seven = new NodeTree(7,null,null);
+		NodeTree six = new NodeTree(6,null,null);
+		NodeTree five = new NodeTree(5,null,null);
+		NodeTree four = new NodeTree(4,null,null);
+		NodeTree two = new NodeTree(2,seven,six);
+		NodeTree three = new NodeTree(3,five,four);
+		NodeTree root = new NodeTree(1,two,three);
+		printLevelOrder(root);
+		printSpiral(root);
+		
+	}
+	
+	
+	static void printLevelOrder(NodeTree root){
+		System.out.println("Printing level order");
+		Queue<NodeTree> queue=new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()){
+			NodeTree temp = queue.poll();
+			if(temp.left !=null)
+				queue.add(temp.left);
+			if(temp.right != null)
+				queue.add(temp.right);
+			System.out.print(temp.data +"|");
+		}
+	}
+	
+	static void printSpiral(NodeTree root){
+		System.out.println("\nPrinting level order in spiral form");
 
+		boolean isTrue=true;
+		Queue<NodeTree> queue=new LinkedList<>();
+		Stack<NodeTree> stack=new Stack<>(); 
+		stack.add(root);
+		while(!queue.isEmpty() || !stack.isEmpty()){
+			if(!queue.isEmpty()){
+				NodeTree temp = queue.poll();
+				if(temp.left !=null)
+					stack.add(temp.left);
+				if(temp.right != null)
+					stack.add(temp.right);
+				System.out.print(temp.data +"|");
+			}else{
+				NodeTree temp = stack.pop();
+				if(temp.left !=null)
+					queue.add(temp.left);
+				if(temp.right != null)
+					queue.add(temp.right);
+				System.out.print(temp.data +"|");
+			}
+			
+		}
+	}
+	
+	
 }
+class NodeTree{
+	int data;
+	NodeTree left;
+	NodeTree right;
+	NodeTree(int data,NodeTree left,NodeTree right){
+		this.data=data;
+		this.left=left;
+		this.right=right;
+	}
+}
+
